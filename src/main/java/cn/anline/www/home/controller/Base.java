e5dfe4a1aa34b7a8694a924d6d4a0e4d4695477c
Jiankian <luozhengan@vip.qq.com>
2017-08-31 04:13:41 +0800

    实现了前台主页显示

--- src/main/java/cn/anline/www/home/controller/Base.java
+++ src/main/java/cn/anline/www/home/controller/Base.java
@@ -1,14 +1,19 @@
 package cn.anline.www.home.controller;
 
+import act.app.ActionContext;
 import cn.anline.www.AnnBase;
 import org.osgl.mvc.annotation.After;
 import org.osgl.mvc.annotation.Before;
+
+import javax.inject.Inject;
 
 /**
  * 前台总基类
  */
 public class Base extends AnnBase {
 
+    @Inject
+    ActionContext actionContext;
     /**
      * 前台全局构造器
      * 框架构造器
@@ -16,8 +21,26 @@ public class Base extends AnnBase {
     @Before
     public void _init(){
         System.out.println("---前台全局构造器已执行---");
+        actionContext.renderArg("asset",_AssetPath());
     }
 
+    /**
+     * 前台主题资源根路径
+     * 判断是否手机版做决策
+     * @return
+     */
+    public String _AssetPath(){
+        return "/asset/home/star/";
+    }
+
+    /**
+     * 前台主体视图文件路径
+     * 判断是否手机版做决策
+     * @return
+     */
+    public String _ThemePath(){
+        return "/skins/star/";
+    }
     /**
      * 前台全局析构器
      * 框架析构器
--- src/main/java/cn/anline/www/home/controller/Index.java
+++ src/main/java/cn/anline/www/home/controller/Index.java
@@ -14,18 +14,13 @@ public class Index extends Base{
     Theme theme;
     @Before
     public void init(){
-
         System.out.println("---前台index控制器构造器已经执行---");
     }
     @GetAction({"/","/index{a}"})
     public void index(@DefaultValue("安浪科技") String a){
         System.out.println("--前台index控制器index方法已执行---");
-        System.out.println("主题路径："+theme.getThemePath());
-        System.out.println("资源路径："+theme.getAssetPath());
-        String _a = a;
-        this.tpl("index.html",a);
-//        Controller.Util.render("index.html",a);
-
+        System.out.println(_ThemePath()+"index/index.html");
+        this.tpl(_ThemePath()+"index/index.html");
     }
     @After
     public void uninit(){
